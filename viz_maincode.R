78068e088a92186681683145af2cc07aa3935be9
skmorgane <morgan@weecology.org>
2017-02-01 10:41:16 -0500

    changed read.csv to compile_forecasts function
    We altered how the predictions made each month are stored from 1 large file to individual monthly files. A function (compile_forecasts) is now used to put all prediction files into one raw dataframe

--- viz_maincode.R
+++ viz_maincode.R
@@ -3,7 +3,7 @@ source("forecast_tools.R")
 
 source("https://raw.githubusercontent.com/weecology/PortalData/master/DataSummaryScripts/RodentAbundances.R")
 
-data = read.csv("./predictions/PortalForecasts.csv")
+data = compile_forecasts()
 ensemble = make_ensemble(data)
 
 plot_sp_predicts(ensemble)
