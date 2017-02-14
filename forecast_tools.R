1c00ff5ef7194d44a85f83e31c53182caaf1b7d7
Shawn <shawntaylor@weecology.org>
2017-02-14 12:09:01 -0500

    download the entire PortalData repo instead of single files

--- forecast_tools.R
+++ forecast_tools.R
@@ -147,31 +147,45 @@ compile_forecasts=function(forecast_folder='./predictions', verbose=FALSE){
   return(all_forecasts)
 }
 
-#' Download the portal observations
+#' Download the PortalData repo
 #' 
-#' This downloads the portal data regardless if they are
-#' new or not.
+#' This downloads the latest portal data regardless if they are
+#' actually updated or not.
 #' 
 #' TODO: incorperate data retriever into this when it's pointed at the github repo
 #' @return None
-download_observations = function(observations_folder='./observations/'){
-  download_list = c('https://raw.githubusercontent.com/weecology/PortalData/master/Rodents/Portal_rodent.csv',
-                    'https://raw.githubusercontent.com/weecology/PortalData/master/Rodents/Portal_rodent_trapping.csv',
-                    'https://raw.githubusercontent.com/weecology/PortalData/master/Rodents/moon_dates.csv',
-                    'https://raw.githubusercontent.com/weecology/PortalData/master/Rodents/Portal_rodent_species.csv')
+download_observations = function(base_folder='~/'){
+  base_folder=path.expand(base_folder)
+  zip_download_path='https://github.com/weecology/PortalData/archive/master.zip'
+  zip_download_dest=paste0(base_folder,'PortalData.zip')
+  download.file(zip_download_path, zip_download_dest, quiet = TRUE)
+
+  final_data_folder=paste0(base_folder,'PortalData')
   
-  for(file in download_list){
-    download.file(file, paste0(observations_folder, basename(file)), quiet = TRUE)
+  #Clear out the old files in the data folder without doing potentially dangerous
+  #recursive deleting.
+  if(file.exists(final_data_folder)) {
+    old_files=list.files(final_data_folder, full.names = TRUE, all.files = TRUE, recursive = TRUE, include.dirs = FALSE)
+    file.remove(old_files)
+    old_dirs=list.files(final_data_folder, full.names = TRUE, recursive = TRUE, include.dirs = TRUE)
+    file.remove(old_dirs)
   }
   
+  #Github serves this up with the -master extension. Unzip and rename to remove that. 
+  unzip(zip_download_dest, exdir=base_folder)
+  file.remove(zip_download_dest)
+  file.rename(paste0(base_folder,'PortalData-master'), final_data_folder)
 }
 
-#' Check if there are new rodent observations
+#' Check if there are new rodent observations. This only checks the
+#' Portal_rodent.csv file. If other things are updated this function 
+#' will not show that there is new data available.
 #' 
 #' @return bool True if new observations are available
-observations_are_new = function(observations_folder='./observations/'){
-  md5_file = paste0(observations_folder,'Portal_rodent.md5')
-  rodent_file= paste0(observations_folder,'Portal_rodent.csv')
+observations_are_new = function(base_folder='~/'){
+  base_folder=path.expand(base_folder)
+  md5_file = './Portal_rodent.md5'
+  rodent_file= path.expand(paste0(base_folder,'PortalData/Rodents/Portal_rodent.csv'))
   if(!file.exists(rodent_file)) stop('Rodent observations not present. Please run download_observations()')
   
   if(!file.exists(md5_file)) {
