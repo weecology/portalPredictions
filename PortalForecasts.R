431e686fc3dbd96930eb76ba5896300222311bbf
Shawn <shawntaylor@weecology.org>
2017-04-02 18:18:19 -0400

    save aic weights of all models

--- PortalForecasts.R
+++ PortalForecasts.R
@@ -116,7 +116,7 @@ colnames(zero_abund_forecast$interval) = c('lower','upper')
 #####Forecasting wrapper function for all models########################
 
 forecastall <- function(abundances,level,weather_data,weatherforecast) {
-  
+  aic_weights = data.frame()
   
   ##Community level predictions
   
@@ -127,7 +127,8 @@ forecastall <- function(abundances,level,weather_data,weatherforecast) {
                          currency="abundance",model="Forecast", level=level, species="total", estimate=model01$mean, 
                          LowerPI=model01$lower[,which(model01$level==90)], UpperPI=model01$upper[,which(model01$level==90)])
   forecasts01[sapply(forecasts01, is.ts)] <- lapply(forecasts01[sapply(forecasts01, is.ts)],unclass)
-  
+  aic_weights = aic_weights %>%
+    bind_rows(data.frame(date=forecast_date, model='Forecast', currency='abundance', level=level, species='total', aic=model01$model$aic))
   
   model02=forecast(auto.arima(abundances$total,lambda = 0),h=12,level=0.9,fan=T)
   
@@ -135,7 +136,8 @@ forecastall <- function(abundances,level,weather_data,weatherforecast) {
                          currency="abundance", model="AutoArima", level=level, species="total", estimate=model02$mean, 
                          LowerPI=model02$lower[,which(model02$level==90)], UpperPI=model02$upper[,which(model02$level==90)])
   forecasts02[sapply(forecasts02, is.ts)] <- lapply(forecasts02[sapply(forecasts02, is.ts)],unclass)
-  
+  aic_weights = aic_weights %>%
+    bind_rows(data.frame(date=forecast_date, model='AutoArima', currency='abundance', level=level, species='total', aic=model02$model$aic))
   
   #Start builing results table
   forecasts=rbind(forecasts01,forecasts02)
@@ -144,20 +146,26 @@ forecastall <- function(abundances,level,weather_data,weatherforecast) {
   #Note: PI is missing. It has an error in the Poison Env model which produces NA values. 
   species=c('BA','DM','DO','DS','NA','OL','OT','PB','PE','PF','PH','PL','PM','PP','RF','RM','RO','SF','SH','SO','total')
   
+  #Model AIC sometimes doesn't work if species counts are low. In that case give a very large AIC.
   for(s in species) {
     species_abundance = abundances %>% 
       extract2(s)
 
     if(sum(species_abundance) == 0){
       pred = zero_abund_forecast
+      model_aic = 1e6
     } else {
       model=tsglm(species_abundance,model=list(past_obs=1,past_mean=12),distr="nbinom")
       pred=predict(model,12,level=0.9) 
+      model_aic = ifelse(has_error(summary(model))==T,1e6,summary(model)$AIC)
     }
     newpred=data.frame(date=rep(forecast_date,12), forecastmonth=forecast_months,forecastyear=forecast_years,NewMoonNumber=forecast_newmoons,
                        currency="abundance",model=rep("NegBinom Time Series",12),level=level,
                        species=rep(s,12), estimate=pred$pred, LowerPI=pred$interval[,1],UpperPI=pred$interval[,2])
     forecasts=rbind(forecasts,newpred)
+    
+    aic_weights = aic_weights %>%
+      bind_rows(data.frame(date=forecast_date, model='NegBinom Time Series', currency='abundance', level=level, species=s, aic=model_aic))
   }
   
   #Species level time time series model with the best environmental covariates chosen by AIC
@@ -180,35 +188,42 @@ forecastall <- function(abundances,level,weather_data,weatherforecast) {
 
     if(sum(species_abundance) == 0){
       pred = zero_abund_forecast
+      model_aic = 1e6
     } else {
       best_model_aic = Inf
       best_model = NA
       for(proposed_model_covariates in model_covariates){
         proposed_model = tsglm(species_abundance,model=list(past_obs=1,past_mean=12),distr="poisson",xreg=weather_data[,unlist(proposed_model_covariates)],link = "log")
         #tsglm sometimes outputs an error when the time series have many 0's, in that case set the AIC
-        #to Inf to this proposed model covariate set get skipped
+        #to Inf so this proposed model covariate set get skipped
         proposed_model_aic = ifelse(has_error(summary(proposed_model))==T,Inf,summary(proposed_model)$AIC)
         if(proposed_model_aic < best_model_aic){
           best_model = proposed_model
           best_model_aic = proposed_model_aic
         }
       }
-    }
-    #If no best model was chosen, ie. they all had infinit AIC's due to errors in model building
-    #then forecast 0's
-    if(is.na(best_model)){
-      pred = zero_abund_forecast
-    } else {
-      pred = predict(best_model,12,level=0.9,newdata=weathermeans) 
-    }
     
+      #If no best model was chosen, ie. they all had infinit AIC's due to errors in model building
+      #then forecast 0's. Also make an extremely high AIC so this isn't weighted heavily in the ensemble. 
+      if(is.na(best_model)){
+        pred = zero_abund_forecast
+        model_aic = 1e6
+      } else {
+        pred = predict(best_model,12,level=0.9,newdata=weathermeans)
+        model_aic = best_model_aic
+      }
+    }
     newpred = data.frame(date=rep(forecast_date,12), forecastmonth=forecast_months,forecastyear=forecast_years,NewMoonNumber=forecast_newmoons,
                         currency="abundance",model=rep("Poisson Env",12),level=level, 
                         species=rep(s,12), estimate=pred$pred, LowerPI=pred$interval[,1],UpperPI=pred$interval[,2])
     forecasts = rbind(forecasts,newpred)
+    aic_weights = aic_weights %>%
+      bind_rows(data.frame(date=forecast_date, model='Poisson Env', currency='abundance', level=level, species=s, aic=model_aic))
   }
   
   write.csv(forecasts,paste(as.character(forecast_date),level,"forecasts.csv",sep=""),row.names=FALSE) 
+  write.csv(aic_weights,paste(as.character(forecast_date),level,"forecasts_aic_weights.csv",sep=""),row.names=FALSE) 
+  
   return(forecasts)
 }
 
