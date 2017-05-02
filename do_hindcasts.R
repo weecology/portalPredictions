fede8c85e011933d992aaa4d1ca3cc4b7b066af6
skmorgane <morgan@weecology.org>
2017-05-02 15:14:00 -0400

    Update file paths to use FullPath()

--- do_hindcasts.R
+++ do_hindcasts.R
@@ -7,10 +7,13 @@ library(PortalDataSummaries)
 #' like the sampling period in period_info just happened. 
 #' @param period_info data.frame One row data.frame output from new_moons
 #' @return None
-backdate_observed_data=function(period_info){
-  rodents_filename  = '~/PortalData/Rodents/Portal_rodent.csv'
-  new_moon_filename = '~/PortalData/Rodents/moon_dates.csv'
-  weather_filename  = '~/PortalData/Weather/Portal_weather.csv'
+backdate_observed_data=function(period_info, base_path='~'){
+  rodents_filename  = PortalDataSummaries::FullPath('PortalData/Rodents/Portal_rodent.csv', 
+                                                    base_path)
+  new_moon_filename = PortalDataSummaries::FullPath('PortalData/Rodents/moon_dates.csv', 
+                                                    base_path)
+  weather_filename  = PortalDataSummaries::FullPath('PortalData/Weather/Portal_weather.csv', 
+                                                    base_path)
   
   period_info$CensusDate = as.Date(period_info$CensusDate)
   period_info$month      = lubridate::month(period_info$CensusDate)
@@ -41,8 +44,11 @@ initial_time_periods=381:458
 #Get the latest to obtain the latest new moon info
 PortalDataSummaries::download_observations()
 
+base_path = '~'
+new_moon_file = PortalDataSummaries::FullPath('PortalData/Rodents/moon_dates.csv', 
+                                                  base_path)
 
-new_moons=read.csv('~/PortalData/Rodents/moon_dates.csv')
+new_moons=read.csv(new_moon_file)
 
 #Period number, year, and  month of sampling will be used to backdate the observation data
 initial_times_info = new_moons %>%
