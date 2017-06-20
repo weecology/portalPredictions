ca5a59baaab79712016a8046cb800168d14ecfd1
Ethan White <ethan@weecology.org>
2017-06-20 14:59:48 -0400

    Actually install tscount & dependencies

--- install-packages.R
+++ install-packages.R
@@ -6,11 +6,11 @@ if ("pacman" %in% rownames(installed.packages()) == FALSE) install.packages("pac
 # Install analysis packages using pacman
 
 pacman::p_load(devtools, dplyr, forecast, ggplot2, lubridate, readr,
-               tidyverse, zoo, magrittr, rmarkdown)
+               tidyverse, zoo, magrittr, rmarkdown, ltsa, parallel)
 pacman::p_load_gh('weecology/PortalDataSummaries')
 
 # Manually install tscount from CRAN archive
 
 download.file(url = "https://cran.r-project.org/src/contrib/Archive/tscount/tscount_1.3.0.tar.gz",
               destfile = "~/tscount_1.3.0.tar.gz")
-install.packages(pkgs = "pkgFile", type = "source", repos = NULL)
+install.packages(pkgs = "~/tscount_1.3.0.tar.gz", type = "source", repos = NULL)
