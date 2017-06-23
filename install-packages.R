8efbcda4dcad740ad933832d1a6a9eaee4068511
Ethan White <ethan@weecology.org>
2017-06-23 13:15:19 -0400

    Only install tscount if not already installed

--- install-packages.R
+++ install-packages.R
@@ -11,6 +11,8 @@ pacman::p_load_gh('weecology/PortalDataSummaries')
 
 # Manually install tscount from CRAN archive
 
-download.file(url = "https://cran.r-project.org/src/contrib/Archive/tscount/tscount_1.3.0.tar.gz",
-              destfile = "~/tscount_1.3.0.tar.gz")
-install.packages(pkgs = "~/tscount_1.3.0.tar.gz", type = "source", repos = NULL)
+if (!("tscount" %in% rownames(installed.packages()))){
+  download.file(url = "https://cran.r-project.org/src/contrib/Archive/tscount/tscount_1.3.0.tar.gz",
+                destfile = "~/tscount_1.3.0.tar.gz")
+  install.packages(pkgs = "~/tscount_1.3.0.tar.gz", type = "source", repos = NULL)
+}
