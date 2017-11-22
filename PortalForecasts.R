c4284ee9cbc4e7f562cf601e50edbb3c71ce8808
gmyenni <glenda@weecology.org>
2017-11-21 20:24:14 -0700

    Refactor hindcasting to use model plugins

--- PortalForecasts.R
+++ PortalForecasts.R
@@ -1,15 +1,17 @@
 source('tools/model_functions.R')
 source('forecast_tools.R')
 
-#####Run all models########################  
+#########Run all models##################################################  
 cat("Running models", "\n")
 sapply( list.files("models", full.names=TRUE), source ) ###Temporary, while only modeling in R
 
+
+#####Collect all forecast results and save to predictions directory######
 cat("Compiling site level forecasts", "\n")
-allforecasts=forecastall("All",filename_suffix = 'forecasts')
+allforecasts=forecastall("All")
 
 cat("Compiling control plot forecasts", "\n")
-controlsforecasts=forecastall("Controls", filename_suffix = 'forecasts')
+controlsforecasts=forecastall("Controls")
 
-######Update Website####################################################
+######Update Website#####################################################
 rmarkdown::render_site()
