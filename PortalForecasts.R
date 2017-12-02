5f0057a3a3519cf66fe73fe99fd09a82223fc0a5
Glenda Yenni <glenda@weecology.org>
2017-12-02 10:09:44 -0700

    Do not run PortalForecasts.R if data not updated

--- PortalForecasts.R
+++ PortalForecasts.R
@@ -1,5 +1,11 @@
 source('tools/model_functions.R')
 source('tools/forecast_tools.R')
+library(yaml)
+
+model_metadata = yaml.load_file("data/model_metadata.yaml")
+forecast_date = as.Date(model_metadata$forecast_date)
+
+if(!(forecast_date == Sys.Date())){ stop('Data not updated') }
 
 #########Run all models##################################################  
 cat("Running models", "\n")
@@ -8,7 +14,7 @@ sapply( list.files("models", full.names=TRUE), source ) ###Temporary, while only
 
 #####Collect all forecast results and save to predictions directory######
 cat("Compiling forecasts", "\n")
-newforecasts=forecastall(Sys.Date())
+newforecasts=forecastall(forecast_date)
 unlink("tmp/*")
 ######Update Website#####################################################
 rmarkdown::render_site()
