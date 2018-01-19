62167f015b8de0ab191d31323d655154d8427001
Ethan White <ethan@weecology.org>
2018-01-19 09:41:36 -0500

    Only archive weekly forecasts
    We only want to archive forecasts weekly, not every time a change is made to the
    codebase. This avoids lots of similar/identical forecasts resulting from active
    development. Previously we were limiting archiving to once per day. This change
    identifies builds on Travis that are part of the weekly runs and only archives
    those.

--- update_repo.sh
+++ update_repo.sh
@@ -1,6 +1,11 @@
-last_commit_date=`git log --date=iso --author='Weecology Deploy Bot' -1 --format=%cd | head -c 10`
-current_date=`date -I | head -c 10`
-if [ "$last_commit_date" !=  "$current_date" ]; then
+# Archive forecasts by pushing weekly forecasts to GitHub and tagging a
+# release so that the GitHub-Zenodo integration archives the forecasts to
+# Zenodo
+
+# Only releases on cron driven events so that only weekly forecasts and not
+# simple changes to the codebase triggers archiving.
+
+if [ "$TRAVIS_EVENT_TYPE" == "cron" ]; then
     git config --global user.email "weecologydeploy@weecology.org"
     git config --global user.name "Weecology Deploy Bot"
 
@@ -12,6 +17,7 @@ if [ "$last_commit_date" !=  "$current_date" ]; then
     git push --quiet deploy master > /dev/null 2>&1
 
     # Create a new release to trigger automated Zenodo archiving
+    current_date=`date -I | head -c 10`
     git tag $current_date
     git push --quiet deploy --tags > /dev/null 2>&1
     curl -v -i -X POST -H "Content-Type:application/json" -H "Authorization: token $GITHUB_RELEASE_TOKEN" https://api.github.com/repos/weecology/portalPredictions/releases -d "{\"tag_name\":\"$current_date\"}"
