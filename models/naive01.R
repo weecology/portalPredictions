7bb9ecd5fb1c1ddef31e585cb3f071d90ecb8c6c
DAPPERstats <dapperstats@gmail.com>
2018-01-25 01:02:13 -0800

    working on the interpolation for naive1
    digging down into the functions underlying the model to get to the estimation procedures
    still work to be done here

--- models/naive01.R
+++ models/naive01.R
@@ -1,67 +1,110 @@
-###naive model####
+# Naive1
+#
+# Model Naive 1 is a flexible exponential smoothing state space model fit
+#  using the forecast and ets functions with with BoxCox.lambda(0)
+#  and allow.multiplicative.trend = TRUE
 
-#Model 1 is the default Forecast package with BoxCox.lambda(0),allow.multiplicative.trend=T
-library(yaml)
-library(forecast)
+# load dependencies
 
-#Write model function
+  library(forecast)
 
-naive1=function(abundances,forecast_date,forecast_months,forecast_years,forecast_newmoons,level,num_forecast_months = 12, CI_level = .9) {
+# Write model function
 
-# interpolate missing data
+  naive1 <- function(abundances, forecast_date, forecast_months, 
+                     forecast_years, forecast_newmoons, level,
+                     num_forecast_months = 12, CI_level = .9){
 
-moons <- (min(abundances$newmoonnumber)):(max(abundances$newmoonnumber))
-abunds <- rep(NA, length(moons))
-for(i in 1:length(moons)){
-  if(length(which(abundances$newmoonnumber == moons[i])) > 0){
-    abunds[i] <- abundances$total[abundances$newmoonnumber == moons[i]]
+    # interpolate missing data
+
+      moons <- (min(abundances$newmoonnumber)):(max(abundances$newmoonnumber))
+      abunds <- rep(NA, length(moons))
+      for(i in 1:length(moons)){
+        if(length(which(abundances$newmoonnumber == moons[i])) > 0){
+          abunds[i] <- abundances$total[abundances$newmoonnumber == moons[i]]
+        }
+      }
+
+      interpolated_abundances <- na.interp(abunds)
+
+    # fit the ets model and forecast with it
+
+      model01 <- forecast(interpolated_abundances, h = num_forecast_months,
+                          level = CI_level, BoxCox.lambda(0),
+                          allow.multiplicative.trend = TRUE)
+
+    # prep the forecast data tabe
+
+      forecasts01 <- data.frame(date = forecast_date, 
+                                forecastmonth = forecast_months,
+                                forecastyear = forecast_years, 
+                                newmoonnumber = forecast_newmoons,
+                                currency = "abundance",
+                                model = "Forecast", level = level, 
+                                species = "total", estimate = model01$mean,
+                                LowerPI = model01$lower[,
+                                          which(model01$level==CI_level*100)], 
+                                UpperPI = model01$upper[,
+                                          which(model01$level==CI_level*100)])
+       forecasts01[sapply(forecasts01, is.ts)] <- 
+          lapply(forecasts01[sapply(forecasts01, is.ts)], unclass)
+  
+       # Include columns describing the data used in the forecast
+
+         forecasts01$fit_start_newmoon <- min(abundances$newmoonnumber)
+         forecasts01$fit_end_newmoon <- max(abundances$newmoonnumber)
+         forecasts01$initial_newmoon <- max(abundances$newmoonnumber)
+  
+    # prep the aic data tabe
+      
+      aic <- data.frame(date = as.Date(forecast_date), 
+                        currency = 'abundance', 
+                        model = 'Forecast', level = level, species = 'total', 
+                        aic = as.numeric(model01$model$aic), 
+                        fit_start_newmoon = min(abundances$newmoonnumber),
+                        fit_end_newmoon = max(abundances$newmoonnumber), 
+                        initial_newmoon = max(abundances$newmoonnumber))
+
+    # return the output
+
+      return(list(forecasts01,aic))
   }
-}
-
-interpolated_abundances <- na.interp(abunds)
 
 
-model01=forecast(interpolated_abundances,h=num_forecast_months,level=CI_level,BoxCox.lambda(0),allow.multiplicative.trend=T)
+# Run model on all plots and just controls
 
-forecasts01=data.frame(date=forecast_date, forecastmonth=forecast_months,forecastyear=forecast_years, newmoonnumber=forecast_newmoons,
-                         currency="abundance",model="Forecast", level=level, species="total", estimate=model01$mean,
-                         LowerPI=model01$lower[,which(model01$level==CI_level*100)], UpperPI=model01$upper[,which(model01$level==CI_level*100)])
-  forecasts01[sapply(forecasts01, is.ts)] <- lapply(forecasts01[sapply(forecasts01, is.ts)],unclass)
-  
-  #########Include columns describing the data used in the forecast###############
-  forecasts01$fit_start_newmoon = min(abundances$newmoonnumber)
-  forecasts01$fit_end_newmoon   = max(abundances$newmoonnumber)
-  forecasts01$initial_newmoon   = max(abundances$newmoonnumber)
-  
-  aic = data.frame(date=as.Date(forecast_date), currency='abundance', model='Forecast', level=level, species='total', 
-                   aic = as.numeric(model01$model$aic), fit_start_newmoon = min(abundances$newmoonnumber),
-                    fit_end_newmoon = max(abundances$newmoonnumber), initial_newmoon = max(abundances$newmoonnumber))
+  # Get data
 
-  return(list(forecasts01,aic))
-}
+    all <- read.csv("data/rodent_all.csv")
+    controls <- read.csv("data/rodent_controls.csv")
+    model_metadata <- yaml.load_file("data/model_metadata.yaml")
+    forecast_date <- as.Date(model_metadata$forecast_date)
+    filename_suffix <- model_metadata$filename_suffix
+    forecast_months <- model_metadata$forecast_months
+    forecast_years <- model_metadata$forecast_years
+    forecast_newmoons <- model_metadata$forecast_newmoons
 
-#Run model on all plots and just controls
+  # Forecast All plots
 
-#Get data
-all = read.csv("data/rodent_all.csv")
-controls = read.csv("data/rodent_controls.csv")
-model_metadata = yaml.load_file("data/model_metadata.yaml")
-forecast_date = as.Date(model_metadata$forecast_date)
-filename_suffix = model_metadata$filename_suffix
-forecast_months = model_metadata$forecast_months
-forecast_years = model_metadata$forecast_years
-forecast_newmoons = model_metadata$forecast_newmoons
+    allresults <- naive1(all, forecast_date, forecast_months, forecast_years,
+                         forecast_newmoons, "All")
 
-#Forecast All plots
-allresults = naive1(all,forecast_date,forecast_months,forecast_years,forecast_newmoons,"All")
+  # Forecast Control plots
 
-#Forecast Control plots
-controlsresults = naive1(controls,forecast_date,forecast_months,forecast_years,forecast_newmoons,"Controls")
+    controlsresults <- naive1(controls, forecast_date, forecast_months, 
+                              forecast_years, forecast_newmoons, "Controls")
 
-#Combine
-forecasts = bind_rows(allresults[1],controlsresults[1])
-forecast_aics = bind_rows(allresults[2],controlsresults[2])
+  # Combine output
 
-#Write results
-write.csv(forecasts,file.path('tmp', paste("naive1", filename_suffix, ".csv", sep="")),row.names = FALSE)
-write.csv(forecast_aics,file.path('tmp', paste("naive1", filename_suffix, "_model_aic.csv", sep="")),row.names = FALSE)
+    forecasts <- bind_rows(allresults[1], controlsresults[1])
+    forecast_aics <- bind_rows(allresults[2], controlsresults[2])
+
+# Write results out
+
+  write.csv(forecasts, 
+            file.path('tmp', 
+                paste("naive1", filename_suffix, ".csv", sep = "")),
+            row.names = FALSE)
+  write.csv(forecast_aics, 
+            file.path('tmp', 
+                paste("naive1", filename_suffix, "_model_aic.csv", sep = "")),
+            row.names = FALSE)
--- working_space.R
+++ working_space.R
@@ -1,33 +1,94 @@
+source('tools/model_functions.R')
+source('tools/forecast_tools.R')
+library(yaml)
 
-model_1=forecast(auto.arima(abundances$total,lambda = 0),h=num_forecast_months,level=CI_level,fan=T)
-model_2=forecast(auto.arima(abunds,lambda = 0),h=num_forecast_months,level=CI_level,fan=T)
-model_3=forecast(auto.arima(interpolated_abundances,lambda = 0),h=num_forecast_months,level=CI_level,fan=T)
+model_metadata = yaml.load_file("data/model_metadata.yaml")
+forecast_date = as.Date(model_metadata$forecast_date)
 
 
-# interpolate missing data
+  # Get data
 
-moons <- (min(abundances$newmoonnumber)):(max(abundances$newmoonnumber))
-abunds <- rep(NA, length(moons))
-for(i in 1:length(moons)){
-  if(length(which(abundances$newmoonnumber == moons[i])) > 0){
-    abunds[i] <- abundances$total[abundances$newmoonnumber == moons[i]]
+    all <- read.csv("data/rodent_all.csv")
+    controls <- read.csv("data/rodent_controls.csv")
+    model_metadata <- yaml.load_file("data/model_metadata.yaml")
+    forecast_date <- as.Date(model_metadata$forecast_date)
+    filename_suffix <- model_metadata$filename_suffix
+    forecast_months <- model_metadata$forecast_months
+    forecast_years <- model_metadata$forecast_years
+    forecast_newmoons <- model_metadata$forecast_newmoons
+
+  # Forecast All plots
+
+    allresults <- naive1(all, forecast_date, forecast_months, forecast_years,
+                         forecast_newmoons, "All")
+
+
+abundances = all
+forecast_date
+forecast_months
+forecast_years
+forecast_newmoons
+level = "All"
+num_forecast_months = 12
+CI_level = .9
+
+
+
+    # interpolate missing data
+
+      moons <- (min(abundances$newmoonnumber)):(max(abundances$newmoonnumber))
+      abunds <- rep(NA, length(moons))
+      for(i in 1:length(moons)){
+        if(length(which(abundances$newmoonnumber == moons[i])) > 0){
+          abunds[i] <- abundances$total[abundances$newmoonnumber == moons[i]]
+        }
+      }
+
+      interpolated_abundances <- round(na.interp(abunds))
+    
+    # convert to appropriate frequency (12) for seasonality in 
+
+    # fit the ets model and forecast with it
+
+      model01 <- forecast(interpolated_abundances, h = num_forecast_months,
+                          level = CI_level, 
+                          allow.multiplicative.trend = TRUE)
+
+    # prep the forecast data tabe
+
+      forecasts01 <- data.frame(date = forecast_date, 
+                                forecastmonth = forecast_months,
+                                forecastyear = forecast_years, 
+                                newmoonnumber = forecast_newmoons,
+                                currency = "abundance",
+                                model = "Forecast", level = level, 
+                                species = "total", estimate = model01$mean,
+                                LowerPI = model01$lower[,
+                                          which(model01$level==CI_level*100)], 
+                                UpperPI = model01$upper[,
+                                          which(model01$level==CI_level*100)])
+       forecasts01[sapply(forecasts01, is.ts)] <- 
+          lapply(forecasts01[sapply(forecasts01, is.ts)], unclass)
+  
+       # Include columns describing the data used in the forecast
+
+         forecasts01$fit_start_newmoon <- min(abundances$newmoonnumber)
+         forecasts01$fit_end_newmoon <- max(abundances$newmoonnumber)
+         forecasts01$initial_newmoon <- max(abundances$newmoonnumber)
+  
+    # prep the aic data tabe
+      
+      aic <- data.frame(date = as.Date(forecast_date), 
+                        currency = 'abundance', 
+                        model = 'Forecast', level = level, species = 'total', 
+                        aic = as.numeric(model01$model$aic), 
+                        fit_start_newmoon = min(abundances$newmoonnumber),
+                        fit_end_newmoon = max(abundances$newmoonnumber), 
+                        initial_newmoon = max(abundances$newmoonnumber))
+
+    # return the output
+
+      return(list(forecasts01,aic))
   }
-}
-
-interpolated_abundances <- na.interp(abunds)
 
 
-species_abundance
-
-abunds <- rep(NA, length(moons))
-for(i in 1:length(moons)){
-  if(length(which(abundances$newmoonnumber == moons[i])) > 0){
-    abunds[i] <- species_abundance[abundances$newmoonnumber == moons[i]]
-  }
-}
-interpolated_abundances <- na.interp(abunds)
-
-
-      model=tsglm(species_abundance,model=list(past_obs=1,past_mean=12),distr="nbinom", link = "log")
-      model2=tsglm(abunds,model=list(past_obs=1,past_mean=12),distr="nbinom", link = "log")
-      model3=tsglm(interpolated_abundances,model=list(past_obs=1,past_mean=12),distr="nbinom", link = "log")
