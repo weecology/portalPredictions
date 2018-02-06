96271600fd8596ca6ad25f2ed0f999ebae55d6f1
DAPPERstats <dapperstats@gmail.com>
2018-02-06 00:49:34 -0800

    create the main model file
    to replace the pois_env_ts script

---
+++ models/pevgarch.R
@@ -0,0 +1,8 @@
+# pevGARCH
+#
+# Model "pevGARCH" is a generalized autoregresive conditional 
+#  heteroscedasticity model with a Poisson response variable
+#  
+
+  library(tscount)
+  library(forecast)
