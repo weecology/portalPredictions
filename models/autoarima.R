df295b42aefa07398189c075df0916c23810d204
DAPPERstats <dapperstats@gmail.com>
2018-02-05 17:30:25 -0800

    beginning edits

--- models/autoarima.R
+++ models/autoarima.R
@@ -1,6 +1,6 @@
-# Weecology-ARIMA
+# AutoARIMA
 #
-# Model "Weecology-ARIMA" is a flexible auto-regressive integrated moving
+# Model "AutoARIMA" is a flexible auto-regressive integrated moving
 #  average model fit to the data using the auto.arima and forecast functions.
 #  Unfortunately because of the seasonality and sampling occurring with 
 #  different frequencies, which the auto.arima function cannot accommodate, 
@@ -9,56 +9,29 @@
 #  used currently cannot, so to provide an appropriate comparison, we 
 #  interpolate missing data here as well.
 
-
-# load dependencies
-
   library(forecast)
 
-# Write model function
+#' Function for AutoARIMA
+#'
+#' @param abundances table of rodent abundances and time measures
+#' @param forecast_date the dates to be forecast from
+#' @param forecast_months the months of the dates to be forecast
+#' @param forecast_years the years of the dates to be forecast
+#' @param forecast_newmoons the numbers of the new moons to be forecast
+#' @param level name of the type of plots included ("All" or "Controls")
+#' @param num_forecast_newmoons number of new moons to forecast
+#' @param CI_level confidence interval level used for forecast envelope
+#' @return list of forecast and aic tables
 
-  weecology.arima <- function(abundances, forecast_date, forecast_months, 
-                              forecast_years, forecast_newmoons, level,
-                              num_forecast_months = 12, CI_level = 0.9){
+  forecast_autoarima <- function(abundances, forecast_date, forecast_months, 
+                                 forecast_years, forecast_newmoons, level,
+                                 num_forecast_newmoons = 12, CI_level = 0.9){
 
-    # interpolate missing data
-    #  note that we interpolate based on the species-level counts and then
-    #  sum (rather than interpolate the sum) because of Jensen's inequality
-    #  and that we want our models that work with individual species to be
-    #  the same total abundance as our models that just use total.
-    #  [FYI a comparison found that an interpolation of just totals could be
-    #   off by as much as 6%
+    interpolated_abundances <- interpolate_abundance(abundances)
 
-      moons <- (min(abundances$newmoonnumber)):(max(abundances$newmoonnumber))
-      nmoons <- length(moons)
-
-      species <- c("BA", "DM", "DO", "DS", "NA.", "OL", "OT", "PB", "PE", 
-                   "PF", "PH", "PL", "PM", "PP", "RF", "RM", "RO", "SF",
-                   "SH", "SO")
-      nspecies <- length(species)
-
-      abunds <- matrix(NA, nrow = nmoons, ncol = nspecies)
-
-      for(i in 1:nmoons){
-        if(length(which(abundances$newmoonnumber == moons[i])) > 0){
-          abundst <- abundances[which(abundances$newmoonnumber == moons[i]),
-                                which(colnames(abundances) %in% species)]
-          abunds[i, ] <- as.numeric(abundst)
-        }
-      }
-
-      interpolated_abunds <- abunds
-
-      for(j in 1:nspecies){
-        interpolated_abunds[ , j] <- round(na.interp(abunds[, j]))
-      }
-
-      interpolated_total <- apply(interpolated_abunds, 1, sum)
-
-    # fit the arima model and forecast with it
-
-      aam <- auto.arima(interpolated_total, lambda = 0)
-      aamf <- forecast(aam, h = num_forecast_months,
-                        level = CI_level, fan = TRUE)
+    aa_model <- auto.arima(interpolated_abundances$total, lambda = 0)
+    aa_forecast <- forecast(aa_model, h = num_forecast_newmoons,
+                            level = CI_level, fan = TRUE)
 
     # prep the forecast data tabe
 
@@ -121,7 +94,7 @@
                                   forecast_years = forecast_years,
                                   forecast_newmoons = forecast_newmoons,
                                   level = "All",
-                                  num_forecast_months = 12, CI_level = 0.9)
+                                  num_forecast_newmoons = 12, CI_level = 0.9)
 
   # Forecast Control plots
 
@@ -131,7 +104,7 @@
                                        forecast_years = forecast_years,
                                        forecast_newmoons = forecast_newmoons,
                                        level = "Controls",
-                                       num_forecast_months = 12, 
+                                       num_forecast_newmoons = 12, 
                                        CI_level = 0.9)
 
 
