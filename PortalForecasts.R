55aa3352cf7d9fbbd493f89972dfd57a11cfdb48
Glenda Yenni <glenda@weecology.org>
2018-12-03 14:55:31 -0700

    Fix paths for testing

--- PortalForecasts.R
+++ PortalForecasts.R
@@ -4,7 +4,6 @@ library(portalcasting)
 setup_dir(options_all = all_options(main = "portalPredictions", download_existing_predictions = FALSE))
 
 #Run all models using portalcasting defaults
-#Save predictions to predictions directory in base directory
 
 portalcast(options_all = all_options(main = "portalPredictions"))
 
