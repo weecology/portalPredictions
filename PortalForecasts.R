c27364cb0a0064ab717ef54f3f57f6e71ec8e523
DAPPERstats <dapperstats@gmail.com>
2018-12-14 21:15:54 -0800

    pointing base to "." and including a line to remove PortalData once it's been used

--- PortalForecasts.R
+++ PortalForecasts.R
@@ -1,11 +1,12 @@
 library(portalcasting)
 
 #Update data and models
-setup_dir(options_all = all_options(main = "", download_existing_predictions = FALSE))
+setup_dir(options_all = all_options(base = ".", main = "", download_existing_predictions = FALSE))
+unlink(sub_paths(dirtree(base = ".", main = "", subs = "PortalData")), recursive = TRUE, force = TRUE)
 
 #Run all models using portalcasting defaults
 
-portalcast(options_all = all_options(main = ""))
+portalcast(options_all = all_options(base = ".", main = ""))
 message("models done")
 
 #Update Website
