f4a8d716f7d44d61730a18ad30f2274afc14ea01
DAPPERstats <dapperstats@gmail.com>
2018-12-14 19:42:03 -0800

    set main to ""

--- PortalForecasts.R
+++ PortalForecasts.R
@@ -1,11 +1,11 @@
 library(portalcasting)
 
 #Update data and models
-setup_dir(options_all = all_options(main = "portalPredictions", download_existing_predictions = FALSE))
+setup_dir(options_all = all_options(main = "", download_existing_predictions = FALSE))
 
 #Run all models using portalcasting defaults
 
-portalcast(options_all = all_options(main = "portalPredictions"))
+portalcast(options_all = all_options(main = ""))
 message("models done")
 
 #Update Website
