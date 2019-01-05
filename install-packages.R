b39b81a238d44b28e2899799384b0da79a6f2890
DAPPERstats <dapperstats@gmail.com>
2019-01-05 09:03:09 +0200

    formatting

--- install-packages.R
+++ install-packages.R
@@ -1,12 +1,15 @@
 # Install pacman if it isn't already installed
 
-if ("pacman" %in% rownames(installed.packages()) == FALSE) install.packages("pacman")
+if ("pacman" %in% rownames(installed.packages()) == FALSE){
+  install.packages("pacman")
+}
 
 
 # Install analysis packages using pacman
 
-pacman::p_load(devtools, dplyr, forecast, ggplot2, htmltab, lubridate, ltsa, magrittr, 
-               parallel, RCurl, readr, rmarkdown, testthat, tidyverse, tscount, yaml, zoo)
+pacman::p_load(devtools, dplyr, forecast, ggplot2, htmltab, lubridate, ltsa, 
+               magrittr, parallel, RCurl, readr, rmarkdown, testthat, tidyverse,
+               tscount, yaml, zoo)
 pacman::p_load_gh('weecology/portalr')
 #pacman::p_load_gh('weecology/portalcasting')
 devtools::install_github('weecology/portalcasting')
