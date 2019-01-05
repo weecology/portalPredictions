ab8d0795602e145db4a56fb61f5db4517a97ba93
DAPPERstats <dapperstats@gmail.com>
2019-01-04 20:10:08 -0800

    printing some things to test

--- PortalForecasts.R
+++ PortalForecasts.R
@@ -3,6 +3,17 @@ library(portalcasting)
 #Update data and models
 setup_dir()
 
+###########
+tree <- dirtree(".", "", c("predictions", "models", "PortalData", "data", "tmp"));
+all <- read.csv(file_path(tree, "data/all.csv"));
+controls <- read.csv(file_path(tree, "data/controls.csv"));
+covariates <- read.csv(file_path(tree, "data/covariates.csv")); 
+metadata <- yaml::yaml.load_file(file_path(tree, "data/metadata.yaml"));
+print(covariates)
+cat(metadata$forecast_date)
+cat("\n")
+###
+
 #Run all models using portalcasting defaults
 portalcast()
 
