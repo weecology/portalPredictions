9372d713882c0a56675c19960880efb611ad23a1
DAPPERstats <dapperstats@gmail.com>
2019-01-13 13:18:13 -0800

    Update PortalForecasts.R

--- PortalForecasts.R
+++ PortalForecasts.R
@@ -1,13 +1,13 @@
 library(portalcasting)
 
-#Update data and models
+# Update data and models
 setup_dir(all_options(from_zenodo=F))
-
-#Run all models using portalcasting defaults
+ 
+# Run all models using portalcasting defaults
 portalcast()
 
-#Update Website
+# Update Website
 rmarkdown::render_site()
 
-#Clean up temporary files
+# Clean up temporary files
 cleanup_dir()
