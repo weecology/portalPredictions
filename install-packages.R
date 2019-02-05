95d0a3202cb513d55da886d683c1277e85ddf391
DAPPERstats <dapperstats@gmail.com>
2019-02-04 21:58:16 -0800

    Update install-packages.R

--- install-packages.R
+++ install-packages.R
@@ -10,7 +10,5 @@ if ("pacman" %in% rownames(installed.packages()) == FALSE){
 pacman::p_load(devtools, dplyr, forecast, ggplot2, htmltab, lubridate, ltsa, 
                magrittr, parallel, RCurl, readr, rmarkdown, testthat, tidyverse,
                tscount, yaml, zoo)
-#pacman::p_load_gh('weecology/portalr')
-#pacman::p_load_gh('weecology/portalcasting')
-devtools::install_github('weecology/portalr')
-devtools::install_github('weecology/portalcasting')
+pacman::p_load_gh('weecology/portalr')
+pacman::p_load_gh('weecology/portalcasting')
