33c817e61d9361f70a485e5b149a02996e182458
Juniper Simonis <dapperstats@gmail.com>
2019-03-17 02:43:05 -0700

    Update install-packages.R
    switching the order so that portalcasting is installed first to avoid the weird repo clashing that is happening when i try to build locally
    also removing the use of the dev version of portalr (portalcasting is set with the stable cran version now too)

--- install-packages.R
+++ install-packages.R
@@ -4,11 +4,10 @@ if ("pacman" %in% rownames(installed.packages()) == FALSE){
   install.packages("pacman")
 }
 
-
 # Install analysis packages using pacman
 
+pacman::p_load_gh('weecology/portalcasting')
 pacman::p_load(devtools, dplyr, forecast, ggplot2, htmltab, lubridate, ltsa, 
                magrittr, parallel, RCurl, readr, rmarkdown, testthat, tidyverse,
-               tscount, yaml, zoo)
-pacman::p_load_gh('weecology/portalr')
-pacman::p_load_gh('weecology/portalcasting')
+               tscount, yaml, zoo, portalr)
+
