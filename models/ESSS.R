b46ffe4d32509fad7966218113ba4b768d36dfbd
Juniper Simonis <dapperstats@gmail.com>
2019-03-17 03:02:24 -0700

    updating the models scripts to reflect portalcasting v0.4.0

--- models/ESSS.R
+++ models/ESSS.R
@@ -1,7 +1,4 @@
 tree <- dirtree(".", "", subdirs());
-all <- read_data(tree, "all");
-controls <-read_data(tree, "controls");
-metadata <- read_data(tree, "metadata");
-f_a <- ESSS(all, metadata, quiet = FALSE);
-f_c <- ESSS(controls, metadata, level = "Controls", quiet = FALSE);
-save_forecast_output(f_a, f_c, "ESSS", metadata, sub_path(tree, "tmp"))
+f_a <- ESSS(tree, level = "All", quiet = FALSE);
+f_c <- ESSS(tree, level = "Controls", quiet = FALSE);
+save_forecast_output(f_a, f_c, "", tree)
