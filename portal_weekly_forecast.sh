975f958714447f087750e8cf74f38d95443d190b
Ethan White <ethan@weecology.org>
2019-10-16 14:02:12 -0400

    Add ability to make weekly forecasts using HiperGator
    Run times were getting long so this PR allows running on the UF HPC.
    `portal_weekly_forecast.sh` needs to be (and has been) copied into
    the users home directory and the text in crontab.txt added
    to a cronjob on daemon2.

---
+++ portal_weekly_forecast.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#SBATCH --job-name=portal_weekly_forecast
+#SBATCH --mail-user=ethanwhite@ufl.edu
+#SBATCH --mail-type=FAIL,END
+#SBATCH --ntasks=1
+#SBATCH --mem=2gb
+#SBATCH --time=3:00:00
+#SBATCH --partition=hpg2-compute
+#SBATCH --output=portal_weekly_forecast_log.out
+date;hostname;pwd
+
+source /etc/profile.d/modules.sh
+
+module load git R singularity
+
+rm -f portal_predictions_latest.sif
+rm -rf portalPredictions
+rm -rf forecasts
+singularity pull docker://weecology/portal_predictions
+git clone https://github.com/weecology/portalPredictions.git
+cd portalPredictions
+singularity run ../portal_predictions_latest.sif Rscript install-packages.R
+singularity run ../portal_predictions_latest.sif Rscript PortalForecasts.R
+singularity run ../portal_predictions_latest.sif bash archive_hipergator.sh
