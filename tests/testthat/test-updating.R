9f8f683d72425907bd204b63574005fb272dbaea
Juniper Simonis <dapperstats@gmail.com>
2019-10-31 01:59:34 -0700

    Update test-updating.R

--- tests/testthat/test-updating.R
+++ tests/testthat/test-updating.R
@@ -2,6 +2,6 @@ context("checks that repository is building and updating correctly")
 
 test_that("data have been updated", {
   
-  expect_true(Sys.Date()==as.Date(read_metadata()$cast_date))
+  expect_true(Sys.Date()==as.Date(read_metadata("../../")$cast_date))
   
 })
