94f9ed75c240a001037953491810c7ec4c13d4af
Juniper Simonis <dapperstats@gmail.com>
2019-10-30 22:01:57 -0700

    docker test

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,13 @@
 # Use an official Python runtime as a parent image
 FROM rocker/tidyverse:latest
 
-# Install `curl` since apparently it isn't in the Rocker images
+# Install `curl` and `jags`, which are not in the rocker image
 
-RUN apt-get update && apt-get install -y curl
+RUN apt-get update && apt-get install -y curl 
+RUN apt-get update && apt-get install -y jags
 
 # Copy the install script
-ADD install-packages.R .
+ ADD install-packages.R .
 
 # Install any needed packages specified in requirements.txt
-RUN Rscript install-packages.R
+ RUN Rscript install-packages.R
--- install-packages.R
+++ install-packages.R
@@ -6,8 +6,11 @@ if ("pacman" %in% rownames(installed.packages()) == FALSE){
 
 # Install analysis packages using pacman
 
-pacman::p_load_gh('weecology/portalcasting')
-pacman::p_load(devtools, dplyr, forecast, ggplot2, htmltab, lubridate, ltsa, 
-               magrittr, parallel, RCurl, readr, rmarkdown, testthat, tidyverse,
-               tscount, yaml, zoo, portalr)
 
+#pacman::p_load(devtools, dplyr, forecast, ggplot2, htmltab, lubridate, ltsa, 
+#               magrittr, parallel, RCurl, readr, rmarkdown, testthat, tidyverse, 
+#               runjags, portalr, tscount, viridis, yaml, zoo)
+
+
+#pacman::p_load_gh('ha0ye/rEDM')
+pacman::p_load_gh('weecology/portalcasting')
