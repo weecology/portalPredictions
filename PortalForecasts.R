0c5efe78b2d85f674a22b719a33947c2e714a8e8
Juniper Simonis <dapperstats@gmail.com>
2019-10-31 02:29:26 -0700

    Update PortalForecasts.R

--- PortalForecasts.R
+++ PortalForecasts.R
@@ -1,15 +1,11 @@
 library(portalcasting)
-#library(portalr)
-print(sessionInfo())
 
 #Update data and models
 setup_production()
 
 #Run all models using portalcasting defaults
-portalcast(models = c("simplexEDM"))#, "jags_RW"))
+portalcast(models = c("jags_RW"))
 
 #Update Website
 #rmarkdown::render_site()
 
-#Clean up temporary files 
-#cleanup_dir()
