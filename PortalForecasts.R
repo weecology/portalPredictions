a378ef71e5855348eb23a31b5c9b1ff0a083ef15
Juniper Simonis <dapperstats@gmail.com>
2019-10-31 11:05:50 -0700

    Update PortalForecasts.R

--- PortalForecasts.R
+++ PortalForecasts.R
@@ -4,7 +4,7 @@ library(portalcasting)
 setup_production()
 
 #Run all models using portalcasting defaults
-portalcast(models = c("ESSS"))
+portalcast(models = c("ESSS", "AutoArima"))
 
 #Update Website
 rmarkdown::render_site()
