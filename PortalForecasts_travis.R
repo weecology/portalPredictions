d833853ca5b87d10bb325eb613b0a312638297e5
DAPPERstats <dapperstats@gmail.com>
2019-11-13 19:31:22 -0800

    Update PortalForecasts_travis.R

--- PortalForecasts_travis.R
+++ PortalForecasts_travis.R
@@ -3,8 +3,8 @@ library(portalcasting)
 #Update data and models
 setup_production()
 
-#Run quick models using portalcasting defaults
-portalcast(models = c("ESSS", "AutoArima", "NaiveArima", "nbGARCH", "simplexEDM"))
+#Run faster models using portalcasting defaults
+portalcast(models = c("ESSS", "AutoArima", "NaiveArima", "nbGARCH", "nbsGARCH", "pevGARCH", "simplexEDM"))
 
 #Update Website
 rmarkdown::render_site()
