8e9b172d532d730a0c4e8354210894514018844a
Ethan White (he/him) <ethan@weecology.org>
2021-02-09 16:21:14 -0500

    GitHub actions test

--- tests/testthat/test-travis.R
+++ tests/testthat/test-travis.R
@@ -1,6 +1,8 @@
 library(testthat)
 
 context("checks that forecasts exist for use")
+print("IS THIS THING ON?!?")
+print(list.files())
 
 test_that("cast metadata", {
   metadata <- read.csv("casts/casts_metadata.csv")
