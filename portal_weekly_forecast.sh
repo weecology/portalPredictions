196c4b923bc0f7205a6da8f362653478442fa310
Ethan White <ethan@weecology.org>
2021-02-09 11:16:44 -0500

    Improvements to weekly forecast script
    * More descriptive logging
    * Instead of deleting singularity container force replace if updated
    * Instead of deleting forecasts repo update it

--- portal_weekly_forecast.sh
+++ portal_weekly_forecast.sh
@@ -7,20 +7,35 @@
 #SBATCH --time=3:00:00
 #SBATCH --partition=hpg2-compute
 #SBATCH --output=portal_weekly_forecast_log.out
-date;hostname;pwd
 
+echo "INFO: [$(date "+%Y-%m-%d %H:%M:%S")] Starting Weekly Forecast on $(hostname) in $(pwd)"
+
+echo "INFO [$(date "+%Y-%m-%d %H:%M:%S")] Loading required modules"
 source /etc/profile.d/modules.sh
-
 module load git R singularity
 
-rm -f portal_predictions_latest.sif
-rm -rf forecasts
-singularity pull docker://weecology/portal_predictions
-cd portalPredictions
+echo "INFO [$(date "+%Y-%m-%d %H:%M:%S")] Updating singularlity container"
+singularity pull --force docker://weecology/portal_predictions
+
+echo "INFO [$(date "+%Y-%m-%d %H:%M:%S")] Updating forecasts repository"
+cd forecasts
 git fetch origin
 git reset --hard origin/master
+
+echo "INFO [$(date "+%Y-%m-%d %H:%M:%S")] Updating portalPredictions repository"
+cd ../portalPredictions
+git fetch origin
+git reset --hard origin/master
+
+echo "INFO [$(date "+%Y-%m-%d %H:%M:%S")] Running Portal Forecasts"
 singularity run ../portal_predictions_latest.sif Rscript PortalForecasts.R
+
+echo "INFO [$(date "+%Y-%m-%d %H:%M:%S")] Checking if forecasts were successful"
 # Redirect stderr(2) to stdout(1) if command fails, and exit script with 1
 singularity run ../portal_predictions_latest.sif Rscript tests/testthat/test-successful_forecasts.R > ../testthat.log 2>&1 || exit 1
+
+echo "INFO [$(date "+%Y-%m-%d %H:%M:%S")] Archiving to GitHub and Zenodo"
 singularity run ../portal_predictions_latest.sif bash archive_hipergator.sh
+
+echo "INFO [$(date "+%Y-%m-%d %H:%M:%S")] Checking if archiving to GitHub was successful"
 singularity run ../portal_predictions_latest.sif Rscript tests/testthat/test-forecasts_committed.R > ../testthat.log 2>&1 || exit 1
