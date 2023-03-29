6a7d3a58d0fa7fe389930c8efa906def31fe30f1
juniperlsimonis <dapperstats@gmail.com>
2023-03-29 10:58:57 -0700

    Update test-metadata.R

--- tests/testthat/test-metadata.R
+++ tests/testthat/test-metadata.R
@@ -4,5 +4,5 @@ context("checks that forecasts exist for use")
 
 test_that("cast metadata", {
   metadata <- read.csv("forecasts/casts_metadata.csv")
-  expect_true(is.integer(metadata$cast_id))
+  expect_true(is.numeric(metadata$cast_id))
 })
